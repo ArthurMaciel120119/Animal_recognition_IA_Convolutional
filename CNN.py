ef13b8f2ad9bd34ae7026775d92f3f1db3057a62
Arthurzin da Exatas <84238286+ArthurMaciel120119@users.noreply.github.com>
2023-05-05 13:23:12 -0300

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -4,6 +4,7 @@
     # TECNICAS DE REGULARIZACAO PARA DEIXA A REDE MAIS INTELIGENTE
     
 import os
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.utils import image_dataset_from_directory
 from tensorflow import keras
@@ -11,30 +12,62 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropou
 from tensorflow.keras.layers.experimental.preprocessing import Rescaling
 from tensorflow.keras.callbacks import ModelCheckpoint
 
+
+
 #Verificando versão do tensorflow-GPU
 print(tf.__version__)
 print(tf.config.list_physical_devices('GPU'))
 
 #Criando pastas para receber imagens
 current_dir = "E:\IMAGENS_DATASETS"
+
 train_folder = current_dir + "\\Treino"
 val_folder = current_dir + "\\Validacao"
 test_folder = current_dir + "\\Test"
+
+tamanho_train_folder_gados = len(os.listdir(os.path.join(train_folder,'gado')))
+tamanho_train_folder_semgados = len(os.listdir(os.path.join(train_folder,'semgado')))
+
+tamanho_val_folder_gados = len(os.listdir(os.path.join(val_folder,'gado')))
+tamanho_val_folder_semgados = len(os.listdir(os.path.join(val_folder,'semgado')))
+
+tamanho_test_folder_gados = len(os.listdir(os.path.join(test_folder,'gado')))
+tamanho_test_folder_semgados = len(os.listdir(os.path.join(train_folder,'semgado')))
+
+print('Treino Gado: %s' % tamanho_train_folder_gados)
+print('Treino Sem Gado: %s' % tamanho_train_folder_semgados)
+
+print('Validacao Gado: %s' % tamanho_val_folder_gados)
+print('Validacao Sem Gado: %s' % tamanho_val_folder_semgados)
+
+print('Teste Gado: %s' % tamanho_test_folder_gados)
+print('Teste Sem Gado: %s' % tamanho_test_folder_semgados)
+
+image_width = 160
+image_height = 160
+image_color_channel = 3
+image_color_channel_size = 255
+image_size = (image_width, image_height)
+image_shpae = image_size + (image_color_channel,)
+
+batch_size = 32
+epochs = 5
+learning_rate = 0.0001
 
 class_names = ['SEMGADO', 'COMGADO']
 
 #Fazendo o "Dowload das imagens" para definir as que serão usadas para teste,validacao e treino
 train_dataset = image_dataset_from_directory(train_folder, 
-                                            image_size=(200,200),
-                                            batch_size=32) #quanto em quanto o peso sera ajustado
+                                            image_size = image_size,
+                                            batch_size = batch_size,) #quanto em quanto o peso sera ajustado
 
 validation_dataset = image_dataset_from_directory(val_folder, 
-                                            image_size=(200,200),
-                                            batch_size=32)
+                                            image_size = image_size,
+                                            batch_size = batch_size)
 
 test_dataset = image_dataset_from_directory(test_folder, 
-                                            image_size=(200,200),
-                                            batch_size=32)
+                                            image_size = image_size,
+                                            batch_size = batch_size)
 
 #Visualizar os batchs
 for data_batch, labels_batch in train_dataset:
@@ -43,8 +76,30 @@ for data_batch, labels_batch in train_dataset:
     print(data_batch[0].shape)
     break
 
-#Criando e treinando modelo
-model = keras.Sequential() #API Sequencial -> mais simples, tanto para apredizado quanto funcionamento
+#Modelo ALEXNET
+model = keras.Sequential() 
+
+# model.add (Conv2D(filters=96, kernel_size=11, strides=4, activation='relu')) 
+# model.add (MaxPooling2D(pool_size=3,strides=2)) 
+
+# model.add (Conv2D(filters=256, kernel_size=5, padding='same', activation='relu')) 
+# model.add (MaxPooling2D(pool_size=3,strides=2)) 
+
+# model.add (Conv2D(filters=384, kernel_size=3, padding='same', activation='relu')) 
+# model.add (Conv2D(filters=384, kernel_size=3, padding='same', activation='relu')) 
+# model.add (Conv2D(filters=256, kernel_size=3, padding='same', activation='relu')) 
+# model.add (MaxPooling2D(pool_size=3,strides=2)) 
+
+# model.add (Flatten()) 
+# model.add (Dense(4096, activation="relu"))
+# model.add (Dropout(0.5))
+# model.add (Dense(4096, activation="relu"))
+# model.add (Dropout(0.5))
+
+# model.add (Dense(10))
+
+#Criando meu modelo e treinando ele
+# model = keras.Sequential() #API Sequencial -> mais simples, tanto para apredizado quanto funcionamento
 
 model.add (Rescaling(scale=1.0/255)) #Pegar cada cor da imagem e representar como 0 ou1 , para facilitar calculos etc.
 
@@ -63,29 +118,23 @@ model.add (MaxPooling2D(pool_size=(2,2)))
 model.add (Dropout(0.5))#Evitar overfitting
 
 model.add (Flatten()) #Pegar todas as imagens e bota num vetor com todos os pixels
+model.add (Dense(256, activation="relu"))
+model.add (Dropout(0.5)) 
 model.add (Dense(1, activation="sigmoid")) #Dizer se é ou não gado
 
-model.compile(loss="binary_crossentropy",
-              optimizer="adam",
-              metrics=["accuracy"])
 
-#Salva a melhor fase dos acertos da rede neural, sabe que é a melhor fase da rede por meio do numero de erros e acertos
-#callbacks = [  
-#     ModelCheckpoint(
-#         filepath="model1.keras",
-#         save_best_only=True,
-#         monitor="val_loss"
-#     )
-# ]
+
+model.compile(loss="binary_crossentropy",
+              optimizer=tf.keras.optimizers.Adam(learning_rate = learning_rate),
+              metrics=["accuracy"])
 
 history = model.fit(
      train_dataset,
-     epochs=10,
+     epochs=epochs,
      validation_data=validation_dataset,
  )
 
 #Mostrar os resultados da rede em graficos para analisar estagnação ou overfiting
-import matplotlib.pyplot as plt
 
 accuracy = history.history["accuracy"]
 val_accuracy = history.history["val_accuracy"]
@@ -100,73 +149,41 @@ plt.title("Acurácia de Treino e Validacao")
 plt.legend()
 plt.figure()
 plt.plot(epochs, loss, "r", label="Treino loss")
-plt.plot(epochs, loss, "b", label="Val loss")
+plt.plot(epochs, val_loss, "b", label="Val loss")
 plt.xlabel("Épocas")
 plt.ylabel("%s")
 plt.title("Loss de Treino e Validacao")
 plt.legend()
 plt.show()
 
-model.summary()
 
-# Resultado dos Conjuntos de Teste
-# from tensorflow import keras
-# model = keras.models.load_model("model1.keras")
-# test_loss, test_acc = model.evaluate(test_dataset)
-# print(f"Test accuracy: {test_acc:.3f}")
+test_loss, test_acc = model.evaluate(test_dataset)
+print(f"Acurácia Teste: {test_acc:.3f}")
 
-def plot_dataset_predictions(dataset) :
-    features, labels = dataset.as_numpy_iterator().next()
+#model.summary()
+
+# def plot_dataset_predictions(dataset) :
     
-    predictions = model.predict_on_batch(features).flatten()
-    predictions = tf.where(predictions < 0.5, 0, 1)
+#     features, labels = dataset.as_numpy_iterator().next()
     
-    print('Labels-> %s' % labels)
-    print('Predictions-> %s' % predictions.numpy())
+#     predictions = model.predict_on_batch(features).flatten()
+#     predictions = tf.where(predictions < 0.5, 0, 1)
     
-    plt.gcf().clear()
-    plt.figure(figsize = (15,15))
+#     print('Labels-> %s' % labels)
+#     print('Predictions-> %s' % predictions.numpy())
     
-    for i in range(9) :
+    #plt.gcf().clear()
+    #plt.figure(figsize = (15,15))
+    
+    # for i in range(9):
         
-        plt.subplot(3, 3, i+1)
-        plt.axis('off')
+    #     plt.subplot(3,3,i+1)
+    #     plt.axis('off')
         
-        plt.inshow(features[i].astype('uint8'))
-        plt.title(class_names[predictions[i]])
-        
-plot_dataset_predictions(test_dataset)
-
-#Codigo para testar imagens baixadas por fora
-#Código para mostrar a imagem que sera testada na tela
-
-# def showSingleImage(img, title, size):
-#     fig, axis = plt.subplots(figsize = size)
+    #     plt.imshow(features[i].astype('uint8'))
+    #     plt.title(class_names[predictions[i]])
     
-#     axis.imshow(img, 'gray')
-#     axis.set_title(title,fontdict = {'fontsize': 20, 'fontweight': 'medium'})
-#     plt.show()
+# plot_dataset_predictions(test_dataset)
 
-# import cv2
-
-#path = r'C:\Users\Arthu\OneDrive\Área de Trabalho\FACULDADE\gadin.jpg'
-# gado_img = cv2.imread(path)
-# gado_img = cv2.cvtColor(gado_img, cv2.COLOR_BGR2RGB)
-
-# showSingleImage(gado_img, "Gado", (476 , 708))
-
-#Código que testa a imagem e retorna o resultado
-# from keras.preprocessing import image
-# import numpy as np
-
-# gado_img = image.load_img(path, target_size=(200,200))
-# x = image.img_to_array(gado_img)
-# x = np.expand_dims(x, axis=0)
-# pred = (model.predict(x) > 0,5).astype('int32')[0][0]
-
-# if pred == 1 :
-#     print("GADO")
-# else :
-#     print("SEM GADO")
-    
-# print(model.predict(x))
+# model.save('path/to/model')
+# model = tf.keras.models.load_model('path/to/model')
